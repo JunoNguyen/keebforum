9444f444eaf7b68bf9741f97422e18206acac74a
Juno Nguyen <junonguyen27@gmail.com>
2023-04-06 20:17:41 -0500

    added spring initialzr files

---
+++ src/main/java/com/example/keebforum/KeebforumApplication.java
@@ -0,0 +1,13 @@
+package com.example.keebforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeebforumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeebforumApplication.class, args);
+	}
+
+}
